3db11f5dd750a0f99f42376dc2e06e1f6c4661f8
Frank Edwards <fre9@cornell.edu>
2018-01-23 09:07:35 -0500

    incapcitation version prior to NOT abuse rewrite

--- read.r
+++ read.r
@@ -19,7 +19,11 @@ AFCARS$HISORGIN<-ifelse(AFCARS$HISORGIN==1, "hispanic",
 #   summarise(n=n()) #### this matches kids count for TX
 
 #### Make binary vars for parental incapaciation, for first entries
-AFCARS$incap_index<-(AFCARS$PRTSJAIL==1)|(AFCARS$ABANDMNT==1)|(AFCARS$RELINQSH==1)
+AFCARS$incap_index<-(AFCARS$PRTSJAIL==1)|
+  (AFCARS$ABANDMNT==1)|
+  (AFCARS$RELINQSH==1)|
+  (AFCARS$NOCOPE==1)|
+  (AFCARS$HOUSING==1)
 
 AFCARS$first_entry<-(AFCARS$Entered==1)&(AFCARS$TotalRem==1)
 
@@ -27,16 +31,17 @@ pop<-read_fwf("us.1990_2015.singleages.adjusted.txt",
               col_types = "iciiiiiii",
               fwf_widths(c(4,2,5, 2, 1, 1, 1, 2, 8),
                          c("year", "state", "FIPS", "registry", "race", "hispanic", "sex", "age", "pop")))
+
+state.index<-pop%>%
+  select(state, FIPS)%>%
+  distinct()
+
 pop<-left_join(pop%>%
-                 filter(year>=2005)%>%
-                 filter(year<=2010)%>%
-                 filter(age<=18)%>%
+                 filter(age<18)%>%
                  group_by(year, FIPS, hispanic)%>%
                  summarise(child_pop=sum(pop)),
                pop%>%
-                 filter(year>=2005)%>%
-                 filter(year<=2010)%>%
-                 filter(age>18)%>%
+                 filter(age>=18)%>%
                  group_by(year, FIPS, hispanic)%>%
                  summarise(adult_pop=sum(pop)))
 
@@ -45,6 +50,12 @@ pop<-pop%>%
   mutate(HISORGIN=ifelse(HISORGIN==1, "hispanic",
                 ifelse(HISORGIN==0, "non-hispanic",
                        NA)))
+  
+pop<-pop%>%
+  left_join(state.index)
+
+pop<-pop%>%
+  filter(FIPS!=99999)
 
 s_comm<-read_csv("countySComm.csv")
 s_comm<-s_comm%>%
